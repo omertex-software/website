b527dc6e097d1cc100ac5fd4357afbbbd31c4103
witness024 <shokojiadko@gmail.com>
2020-05-11 19:51:26 +0300

    Changed style in social network buttons

--- src/components/socialNetworks/socialNetworks.js
+++ src/components/socialNetworks/socialNetworks.js
@@ -1,54 +1,26 @@
-import React from "react"
-// import style from "./socialNetworks.module.sass"
+import React, { useState } from "react"
+import style from "./socialNetworks.module.sass"
+import classNames from "../../helpers/classNames"
 
 const SocialNetworks = () => {
+  const [opened, setOpened] = useState(false)
+
+  const changeOpened = () => {
+    setOpened(!opened)
+  }
+
   return (
     <div
-      style={{
-        position: "fixed",
-        zIndex: 10,
-        right: 40,
-        bottom: 40,
-      }}
+      className={classNames(
+        style.social_container,
+        opened ? style.show_items : ""
+      )}
     >
-      <a
-        style={{
-          padding: 10,
-          color: "#fff",
-          backgroundColor: "#0088cc",
-          marginRight: 20,
-          borderRadius: 10,
-          textDecoration: "none",
-        }}
-        href="https://t.me/matvey024"
-      >
-        Telegram
-      </a>
-      <a
-        style={{
-          padding: 10,
-          color: "#fff",
-          backgroundColor: "#4267B2",
-          marginRight: 20,
-          borderRadius: 10,
-          textDecoration: "none",
-        }}
-        href="http://m.me/shokojiadko"
-      >
-        Facebook
-      </a>
-      <a
-        style={{
-          padding: 10,
-          color: "#fff",
-          backgroundColor: "#43d854",
-          borderRadius: 10,
-          textDecoration: "none",
-        }}
-        href="https://wa.me/375292747449"
-      >
-        WhatsApp
-      </a>
+      <button className={style.social_close} onClick={changeOpened} />
+      <a className={style.social_whatsapp} href="https://wa.me/375296524258" />
+      <a className={style.social_telegram} href="https://t.me/promaker1" />
+      <a className={style.social_facebook} href="http://m.me/dkhanevich" />
+      <button className={style.social_message} onClick={changeOpened} />
     </div>
   )
 }
