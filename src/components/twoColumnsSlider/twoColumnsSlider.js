6ca5d4f9f0a13d407a60de87f0e3bee3aa956bc5
witness024 <shokojiadko@gmail.com>
2020-06-16 16:48:51 +0300

    Fix slider

--- src/components/twoColumnsSlider/twoColumnsSlider.js
+++ src/components/twoColumnsSlider/twoColumnsSlider.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useRef } from "react"
 import style from "./twoColumnsSlider.module.sass"
 import Img from "gatsby-image"
 import Slider from "react-slick"
@@ -7,64 +7,67 @@ import next from "../../assets/images/twoColumnsSlider/png_icon_next.png"
 import prev from "../../assets/images/twoColumnsSlider/png_icon_prev.png"
 
 const TwoColumnsSlider = ({ data }) => {
-  const SampleNextArrow = ({ onClick }) => {
+  const ref = useRef({})
+
+  const SampleNextArrow = () => {
     return (
       <div
         className={style.slide_next_btn}
         style={{
           backgroundImage: `url(${next})`,
         }}
-        onClick={onClick}
+        onClick={() => ref.current.slickNext()}
       />
     )
   }
 
-  const SamplePrevArrow = ({ onClick }) => {
+  const SamplePrevArrow = () => {
     return (
       <div
         className={style.slide_prev_btn}
         style={{
           backgroundImage: `url(${prev})`,
         }}
-        onClick={onClick}
+        onClick={() => ref.current.slickPrev()}
       />
     )
   }
 
   const settings = {
     slidesToShow: 2,
-    arrows: true,
+    arrows: false,
     dots: true,
     dotsClass: "orange_slick_dots",
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
     responsive: [
       {
         breakpoint: 810,
         settings: {
           slidesToShow: 1,
-          arrows: false,
         },
       },
     ],
   }
 
   return (
-    <Slider className={style.cases_box} {...settings}>
-      {data.map(item => (
-        <div key={item.title}>
-          <div className={style.slide}>
-            <Img
-              fluid={item.image}
-              className={style.slide_img}
-              alt={item.title}
-            />
-            <h3 className={style.slide_title}>{item.title}</h3>
-            <p className={style.slide_description}>{item.description}</p>
+    <div className={style.cases_content}>
+      <SamplePrevArrow />
+      <Slider ref={ref} className={style.cases_box} {...settings}>
+        {data.map(item => (
+          <div key={item.title}>
+            <div className={style.slide}>
+              <Img
+                fluid={item.image}
+                className={style.slide_img}
+                alt={item.title}
+              />
+              <h3 className={style.slide_title}>{item.title}</h3>
+              <p className={style.slide_description}>{item.description}</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </Slider>
+        ))}
+      </Slider>
+      <SampleNextArrow />
+    </div>
   )
 }
 
